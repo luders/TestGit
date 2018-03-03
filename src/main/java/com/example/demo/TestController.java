705ea0b547837e23294b70997260db0e131f6fb3
ly <349427009@qq.com>
2018-03-03 16:27:57 +0800

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class TestController {
+
+    public static void main(String[] args){
+        String hi = "hello world!";
+        System.out.println(hi);
+    }
+}
